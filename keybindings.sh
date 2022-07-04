0794beed39d10b2c096352418b5d305c0a218605
Kellen Green <me@kellengreen.com>
2022-07-04 22:59:45 +0100

    Create keybindings.sh

---
+++ keybindings.sh
@@ -0,0 +1,5 @@
+mkdir -p ~/Library/KeyBindings
+echo '{
+  "\UF729"  = "moveToBeginningOfLine:"; // HOME
+  "\UF72B"  = "moveToEndOfLine:";       // END
+}' > ~/Library/KeyBindings/DefaultKeyBinding.dict
